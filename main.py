232d6a00c46ff06947bac435e8767b8db264a45b
UnnikrishnanNP <uknp0371@gmail.com>
2022-05-07 21:19:40 +0530

    speech recongnition

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import speech_recognition as sr
 import gtts 
 from playsound import playsound
@@ -17,10 +18,30 @@ def audio_to_text():
     except sr.RequestError:
         print("Request Error")
     return text
+
+def play_sound(text):
+    try:
+        tts = gtts.gTTS(text)
+        temp = "./temp.mp3"
+        tts.save(temp)
+        playsound(temp)
+        os.remove(temp)
+    except AssertionError:
+        print("Assertion error")
+
     
 
 if __name__ == "__main__":
     while True:
         command = audio_to_text()
         if ACTIVATION_COMMAND in command.lower():
-            print("activated")
+            print("Activated")
+            play_sound("What can i do for you?")
+
+            note = audio_to_text()
+            if note:
+                play_sound(note)
+
+
+
+
